5ef51afc3b945c01a984b5acbb6e83ac42fc4491
Daniel Panzer <daniel@cruxcode.nyc>
2019-09-02 16:43:58 -0600

    Initial Commit

---
+++ SudoChess/SudoChess.h
@@ -0,0 +1,19 @@
+//
+//  SudoChess.h
+//  SudoChess
+//
+//  Created by Daniel Panzer on 9/2/19.
+//  Copyright © 2019 CruxCode LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SudoChess.
+FOUNDATION_EXPORT double SudoChessVersionNumber;
+
+//! Project version string for SudoChess.
+FOUNDATION_EXPORT const unsigned char SudoChessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SudoChess/PublicHeader.h>
+
+
